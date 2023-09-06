ff803422b118b3fdc401ac0c211c089f2939b81f
Gustavo Gregianin <gustavo.gregianin@usp.br>
2023-09-06 11:03:00 -0300

    Add HelloTest

---
+++ src/test/java/io/github/gregiagu/HelloTest.java
@@ -0,0 +1,10 @@
+package io.github.gregiagu;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+    @Test
+    void helloTest() {
+        System.out.println("Hello test");
+    }
+}
